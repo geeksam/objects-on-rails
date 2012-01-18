8022ab855d48378cde7f58ab00d214351b162f91
Sam Livingston-Gray <geeksam@gmail.com>
2012-01-17 23:04:07 -0800

    New Rails 3.1.3 app, with .rvmrc too

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ObjectsOnRails::Application.config.secret_token = 'd432d1133a7a695c986b7f2c2e8bb29be695768b2dba6bb965c5932eb11e218d24a4b4179c56849ed5067279e1e13d8350940f0d4dfb616202da1c9d4325c234'
