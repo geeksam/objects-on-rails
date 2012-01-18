fe1c6865030e9f7ae7ea214f80fed65a4969bf1d
Sam Livingston-Gray <geeksam@gmail.com>
2012-01-17 23:15:01 -0800

    Blog#new_post

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,5 +1,7 @@
 class Blog
   attr_reader :entries
+  attr_writer :post_maker
+
   def initialize
     @entries = []
   end
@@ -10,5 +12,17 @@ class Blog
 
   def subtitle
     "The trusted source for drying paint news & opinion"
+  end
+
+  def new_post
+    post_maker.call.tap do |p|
+      p.blog = self
+    end
+  end
+
+private
+
+  def post_maker
+    @post_maker ||= Post.public_method(:new)
   end
 end
--- spec/models/blog_spec.rb
+++ spec/models/blog_spec.rb
@@ -1,5 +1,6 @@
 require 'minitest/autorun'
 require_relative '../../app/models/blog'
+require 'ostruct'
 
 describe Blog do
   before do
@@ -8,5 +9,20 @@ describe Blog do
 
   it "should have no entries" do
     @it.entries.must_be_empty
+  end
+
+  describe "#new_post" do
+    before do
+      @new_post = OpenStruct.new
+      @it.post_maker = ->{ @new_post }
+    end
+
+    it "should return a new post" do
+      @it.new_post.must_equal @new_post
+    end
+
+    it "should set the post's blog reference to itself" do
+      @it.new_post.blog.must_equal(@it)
+    end
   end
 end
